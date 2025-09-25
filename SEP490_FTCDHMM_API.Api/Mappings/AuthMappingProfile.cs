8a2dc7a79c1442e3367bbf9595c3a296936fbc07
QuangKhuong3010 <khuongdqhe176204@fpt.edu.vn>
2025-09-25 16:58:22 +0700

    UpdateAutoMapping

--- SEP490_FTCDHMM_API.Api/Mappings/AuthMappingProfile.cs
+++ SEP490_FTCDHMM_API.Api/Mappings/AuthMappingProfile.cs
@@ -6,6 +6,24 @@ public class AuthMappingProfile : Profile
 {
     public AuthMappingProfile()
     {
+        //register
         CreateMap<APIDtos.AuthDTOs.RegisterDto, ApplicationDtos.AuthDTOs.RegisterDto>();
+
+        //verifyEmail
+        CreateMap<APIDtos.AuthDTOs.OtpVerifyDto, ApplicationDtos.AuthDTOs.OtpVerifyDto>();
+
+        //resentOtp
+        CreateMap<APIDtos.AuthDTOs.ResendOtpDto, ApplicationDtos.AuthDTOs.ResendOtpDto>();
+
+        //login
+        CreateMap<APIDtos.AuthDTOs.LoginDto, ApplicationDtos.AuthDTOs.LoginDto>();
+
+        //changePassword
+        CreateMap<APIDtos.AuthDTOs.ChangePasswordDto, ApplicationDtos.AuthDTOs.ChangePasswordDto>();
+
+        //forgotPassword
+        CreateMap<APIDtos.AuthDTOs.ForgotPasswordRequestDto, ApplicationDtos.AuthDTOs.ForgotPasswordRequestDto>();
+        CreateMap<APIDtos.AuthDTOs.VerifyOtpForPasswordResetDto, ApplicationDtos.AuthDTOs.VerifyOtpForPasswordResetDto>();
+        CreateMap<APIDtos.AuthDTOs.ResetPasswordWithTokenDto, ApplicationDtos.AuthDTOs.ResetPasswordWithTokenDto>();
     }
 }
