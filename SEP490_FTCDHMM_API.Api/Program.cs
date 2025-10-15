e9d4a732457d4b59bc8858077fd0e768a6fba756
HuuTam <tamnguyenvd36@gmail.com>
2025-10-16 00:11:11 +0700

    feat(cors): allow access from Vercel deployment in CORS policy

--- SEP490_FTCDHMM_API.Api/Program.cs
+++ SEP490_FTCDHMM_API.Api/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", "https://sep-490-ftcdhmm-ui.vercel.app")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
