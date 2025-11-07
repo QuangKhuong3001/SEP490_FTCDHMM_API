1e536b3aab930fc7130ec691bbab78e60694ce79
HuuTam <tamnguyenvd36@gmail.com>
2025-11-07 20:34:09 +0700

    refactor(comment): add ParentCommentId and UserId properties to CommentResponse

--- SEP490_FTCDHMM_API.Application/Dtos/CommentDtos/CommentResponse.cs
+++ SEP490_FTCDHMM_API.Application/Dtos/CommentDtos/CommentResponse.cs
@@ -7,6 +7,8 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime CreatedAtUtc { get; set; }
+        public Guid? ParentCommentId { get; set; }
+        public Guid UserId { get; set; }
 
         public List<CommentResponse> Replies { get; set; } = new();
     }
