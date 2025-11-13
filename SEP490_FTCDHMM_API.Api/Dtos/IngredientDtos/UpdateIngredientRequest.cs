67b56fb521b9dba9730d20cddf9e9206853ce2d4
HuuTam <tamnguyenvd36@gmail.com>
2025-11-14 05:39:15 +0700

    feat(ingredient): update validation messages to Vietnamese and enhance input constraints

--- SEP490_FTCDHMM_API.Api/Dtos/IngredientDtos/UpdateIngredientRequest.cs
+++ SEP490_FTCDHMM_API.Api/Dtos/IngredientDtos/UpdateIngredientRequest.cs
@@ -5,13 +5,15 @@ namespace SEP490_FTCDHMM_API.Api.Dtos.IngredientDtos
 {
     public class UpdateIngredientRequest
     {
+        [StringLength(1000, ErrorMessage = "Mô tả không được vượt quá 1000 ký tự")]
         public string? Description { get; set; }
+
         public IFormFile? Image { get; set; }
 
-        [Required(ErrorMessage = "Missing Required Nutrients")]
+        [Required(ErrorMessage = "Vui lòng thêm thành phần dinh dưỡng")]
         public List<NutrientRequest>? Nutrients { get; set; }
 
-        [Required(ErrorMessage = "Missing Ingredient Category")]
+        [Required(ErrorMessage = "Vui lòng chọn phân loại nguyên liệu")]
         public required List<Guid> IngredientCategoryIds { get; set; }
     }
 }
