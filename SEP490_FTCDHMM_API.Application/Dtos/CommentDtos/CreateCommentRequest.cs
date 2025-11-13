5cc00efeb6006e133448372458061369ac9c132e
HuuTam <tamnguyenvd36@gmail.com>
2025-11-14 05:49:06 +0700

    feat(comment): add validation attributes for content in CreateCommentRequest

--- SEP490_FTCDHMM_API.Application/Dtos/CommentDtos/CreateCommentRequest.cs
+++ SEP490_FTCDHMM_API.Application/Dtos/CommentDtos/CreateCommentRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace SEP490_FTCDHMM_API.Application.Dtos.CommentDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEP490_FTCDHMM_API.Application.Dtos.CommentDtos
 {
     public class CreateCommentRequest
     {
+        [Required(ErrorMessage = "Bình luận không được để trống")]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Bình luận phải từ 1 đến 1000 ký tự")]
         public string Content { get; set; } = string.Empty;
+
         public Guid? ParentCommentId { get; set; }
     }
 }
