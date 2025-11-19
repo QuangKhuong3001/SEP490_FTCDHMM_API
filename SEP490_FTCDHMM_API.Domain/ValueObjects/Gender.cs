88152c5ec72decdc37aed3a9c7d56051a476f493
QuangKhuong3010 <khuongdqhe176204@fpt.edu.vn>
2025-11-20 01:57:12 +0700

    remove Gender.Other

--- SEP490_FTCDHMM_API.Domain/ValueObjects/Gender.cs
+++ SEP490_FTCDHMM_API.Domain/ValueObjects/Gender.cs
@@ -1,10 +1,11 @@
-﻿namespace SEP490_FTCDHMM_API.Domain.ValueObjects
+﻿using SEP490_FTCDHMM_API.Shared.Exceptions;
+
+namespace SEP490_FTCDHMM_API.Domain.ValueObjects
 {
     public record Gender(string Value)
     {
         public static readonly Gender Male = new("MALE");
         public static readonly Gender Female = new("FEMALE");
-        public static readonly Gender Other = new("OTHER");
 
         public override string ToString() => Value;
 
@@ -14,7 +15,7 @@
             {
                 "MALE" => Male,
                 "FEMALE" => Female,
-                _ => Other
+                _ => throw new AppException(AppResponseCode.INVALID_ACTION, "Giới tính không hợp lệ.")
             };
         }
     }
