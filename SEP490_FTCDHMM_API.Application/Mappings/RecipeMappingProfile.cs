2e9c06db3f351b3a98d78ec3ee01a1ba1d52f22f
HuuTam <tamnguyenvd36@gmail.com>
2025-12-02 07:22:48 +0700

    feat(mapping): add mapping for RecipeUserTag to DraftUserRecipeTagged

--- SEP490_FTCDHMM_API.Application/Mappings/RecipeMappingProfile.cs
+++ SEP490_FTCDHMM_API.Application/Mappings/RecipeMappingProfile.cs
@@ -5,6 +5,7 @@ using SEP490_FTCDHMM_API.Application.Dtos.RecipeDtos.Rating;
 using SEP490_FTCDHMM_API.Application.Dtos.RecipeDtos.RecipeIngredient;
 using SEP490_FTCDHMM_API.Application.Dtos.RecipeDtos.Recommentdation;
 using SEP490_FTCDHMM_API.Application.Dtos.RecipeDtos.Response;
+using SEP490_FTCDHMM_API.Application.Dtos.RecipeDtos.RecipeUserTagged;
 using SEP490_FTCDHMM_API.Domain.Entities;
 
 namespace SEP490_FTCDHMM_API.Application.Mappings
@@ -46,6 +47,14 @@ namespace SEP490_FTCDHMM_API.Application.Mappings
                 );
 
             CreateMap<RecipeUserTag, DraftRecipeUserTaggedResponse>()
+                .ForMember(dest => dest.Id,
+                    opt => opt.MapFrom(src => src.TaggedUserId))
+                .ForMember(dest => dest.FirstName,
+                    opt => opt.MapFrom(src => src.TaggedUser.FirstName))
+                .ForMember(dest => dest.LastName,
+                    opt => opt.MapFrom(src => src.TaggedUser.LastName));
+
+            CreateMap<RecipeUserTag, DraftUserRecipeTagged>()
                 .ForMember(dest => dest.Id,
                     opt => opt.MapFrom(src => src.TaggedUserId))
                 .ForMember(dest => dest.FirstName,
