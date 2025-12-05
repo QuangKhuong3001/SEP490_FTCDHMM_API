781ed3faf9ea4fc2a412a236cfefb2a8a4c4a580
HuuTam <tamnguyenvd36@gmail.com>
2025-12-06 06:01:56 +0700

    fix(dtos): update error messages for NutrientRequest validation

--- SEP490_FTCDHMM_API.Api/Dtos/NutrientDtos/NutrientRequest.cs
+++ SEP490_FTCDHMM_API.Api/Dtos/NutrientDtos/NutrientRequest.cs
@@ -4,17 +4,16 @@ namespace SEP490_FTCDHMM_API.Api.Dtos.NutrientDtos
 {
     public class NutrientRequest
     {
-        [Required(ErrorMessage = "Missing NutrientId")]
         public Guid NutrientId { get; set; }
 
-        [Range(0, 9999999.999, ErrorMessage = "Min must be between 0 and 9999999.999")]
+        [Range(0, 9999999.999, ErrorMessage = "Giá trị Min phải nằm trong khoảng từ 0 đến 9999999.999")]
         public decimal? Min { get; set; }
 
-        [Range(0, 9999999.999, ErrorMessage = "Max must be between 0 and 9999999.999")]
+        [Range(0, 9999999.999, ErrorMessage = "Giá trị Max phải nằm trong khoảng từ 0 đến 9999999.999")]
         public decimal? Max { get; set; }
 
-        [Required(ErrorMessage = "Missing Median.")]
-        [Range(0, 9999999.999, ErrorMessage = "Median must be between 0 and 9999999.999")]
+        [Required(ErrorMessage = "Thiếu giá trị Median.")]
+        [Range(0, 9999999.999, ErrorMessage = "Giá trị Median phải nằm trong khoảng từ 0 đến 9999999.999")]
         public decimal Median { get; set; }
     }
 }
