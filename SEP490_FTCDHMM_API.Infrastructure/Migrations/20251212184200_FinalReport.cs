65edf8edf10bf7e6061f4825f279b072048e61ac
QuangKhuong3010 <khuongdqhe176204@fpt.edu.vn>
2025-12-13 01:42:42 +0700

    Update Migration

---
+++ SEP490_FTCDHMM_API.Infrastructure/Migrations/20251212184200_FinalReport.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEP490_FTCDHMM_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UploadedById",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "LowerName",
+                table: "Ingredients",
+                newName: "UpperName");
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: new Guid("58c77fe0-a3ba-f1c2-0518-3e8a6cc02696"),
+                column: "CreatedAtUTC",
+                value: new DateTime(2025, 12, 12, 18, 42, 0, 530, DateTimeKind.Utc).AddTicks(5933));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UploadedById",
+                table: "Images",
+                column: "UploadedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UploadedById",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "UpperName",
+                table: "Ingredients",
+                newName: "LowerName");
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: new Guid("58c77fe0-a3ba-f1c2-0518-3e8a6cc02696"),
+                column: "CreatedAtUTC",
+                value: new DateTime(2025, 12, 11, 18, 41, 1, 578, DateTimeKind.Utc).AddTicks(795));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UploadedById",
+                table: "Images",
+                column: "UploadedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
