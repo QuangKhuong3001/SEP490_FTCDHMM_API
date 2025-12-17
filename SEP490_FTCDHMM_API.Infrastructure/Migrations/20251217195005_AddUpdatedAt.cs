f1894d7a96c38199cd1a83bb5aa15e76f1624376
QuangKhuong3010 <khuongdqhe176204@fpt.edu.vn>
2025-12-18 02:54:49 +0700

    Update migrtion for user entity

---
+++ SEP490_FTCDHMM_API.Infrastructure/Migrations/20251217195005_AddUpdatedAt.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEP490_FTCDHMM_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: new Guid("58c77fe0-a3ba-f1c2-0518-3e8a6cc02696"),
+                column: "CreatedAtUTC",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(5518));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("133554ee-b8bf-0518-a055-4097baea7b64"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7838));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("16a7239f-04ef-4ae8-3c3d-f7c91f625ade"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7849));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("19f3c506-46ad-f9be-3a10-63dc2ed6a57e"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7860));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("8443f632-4d26-96c3-6c99-cdb180c761f3"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7854));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("b6cb3448-5f59-44b8-e69e-5a2e408ccd97"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7865));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("c8f90ed8-cc93-7d51-8477-534ff99d0fd0"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7870));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("d238ef58-09be-5176-f430-16cdbfc0032a"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7875));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("d5caeabc-0ca2-b778-f234-d5c084dd23cb"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7844));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("edae6e4e-e3a4-ccd4-a2d4-81edf652d3f4"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7828));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("f4a0ea3a-98b1-3443-4739-f63803a841c8"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(7881));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00edafe3-b047-5980-d0fa-da10f400c1e5"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(5832));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("1d6026ce-0dac-13ea-8b72-95f02b7620a7"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(5891));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("8ea665ca-b310-5ac6-c897-ff8b89f9f728"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 19, 50, 5, 237, DateTimeKind.Utc).AddTicks(5881));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedUtc",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: new Guid("58c77fe0-a3ba-f1c2-0518-3e8a6cc02696"),
+                column: "CreatedAtUTC",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(6108));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("133554ee-b8bf-0518-a055-4097baea7b64"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8229));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("16a7239f-04ef-4ae8-3c3d-f7c91f625ade"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8244));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("19f3c506-46ad-f9be-3a10-63dc2ed6a57e"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8255));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("8443f632-4d26-96c3-6c99-cdb180c761f3"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8250));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("b6cb3448-5f59-44b8-e69e-5a2e408ccd97"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8261));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("c8f90ed8-cc93-7d51-8477-534ff99d0fd0"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8267));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("d238ef58-09be-5176-f430-16cdbfc0032a"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8272));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("d5caeabc-0ca2-b778-f234-d5c084dd23cb"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8235));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("edae6e4e-e3a4-ccd4-a2d4-81edf652d3f4"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8220));
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "Id",
+                keyValue: new Guid("f4a0ea3a-98b1-3443-4739-f63803a841c8"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(8278));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00edafe3-b047-5980-d0fa-da10f400c1e5"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(6380));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("1d6026ce-0dac-13ea-8b72-95f02b7620a7"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(6428));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("8ea665ca-b310-5ac6-c897-ff8b89f9f728"),
+                column: "LastUpdatedUtc",
+                value: new DateTime(2025, 12, 17, 15, 7, 8, 631, DateTimeKind.Utc).AddTicks(6421));
+        }
+    }
+}
