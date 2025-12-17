22c1c80cc73475fc2826771069bb22bd94776a47
QuangKhuong3010 <khuongdqhe176204@fpt.edu.vn>
2025-12-18 02:49:23 +0700

    Nothing

--- SEP490_FTCDHMM_API.Api/Mappings/UserMappingProfile.cs
+++ SEP490_FTCDHMM_API.Api/Mappings/UserMappingProfile.cs
@@ -10,7 +10,6 @@ namespace SEP490_FTCDHMM_API.Api.Mappings
         {
             //user manangement
             CreateMap<APIDtos.UserDtos.LockRequest, ApplicationDtos.UserDtos.LockRequest>();
-            CreateMap<APIDtos.UserDtos.CreateModeratorAccountRequest, ApplicationDtos.UserDtos.CreateModeratorAccountRequest>();
 
             //profile
             CreateMap<APIDtos.UserDtos.UpdateProfileRequest, ApplicationDtos.UserDtos.UpdateProfileRequest>();
